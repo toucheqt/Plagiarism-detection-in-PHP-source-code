aef0adce58a127ae874fe27c6c782acd675d3f48
toucheqt <xkrpecqt@gmail.com>
2015-03-13 17:10:43 +0100

    Added startup script

---
+++ src/utils/startup.sh
@@ -0,0 +1,11 @@
+##### @Author: Ondrej Krpec, xkrpecqt@gmail.com #####
+##### @Description: Script for simple running php plagiarism detection tool ####
+#
+#
+clear
+#
+#
+##### HELP #####
+#php ./../controllers/Controller.php --help
+##### BASIC STARTUP #####
+php ./controllers/Controller.php --workingDir ./../tests/ --templateDir ./../tests/template
